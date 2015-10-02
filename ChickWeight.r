ba03535ef745f908c0f2de92aa501db3c50f1451
codykoebnick <codykoebnick@gmail.com>
2015-10-02 14:59:19 +0200

    Update ChickWeight.r

--- ChickWeight.r
+++ ChickWeight.r
@@ -1,20 +1,26 @@
 ## Assignment 1
 ## Cody Koebnick and Rafael Palomino
-## File 2
+## File 2, second data set
 
 library(MASS)
-data()
 
+## Loading the data
 data(ChickWeight)
-summary(ChickWeight)
+
+## Reading about the data
 ?ChickWeight
+
+## Viewing the data
+summary(ChickWeight)
 View(ChickWeight)
 
+## Checking the variance within the data
 var(ChickWeight)
 
+## A quick histogram to visualize the basics of the dependence variable
 hist(ChickWeight$weight)
 
-## plot graph shows that time and chicken weight are positively correlated
+## Plot graph shows that variables Time and Weight are positively correlated
 plot(ChickWeight$Time, ChickWeight$weight)
 
 #attempt at a dynamic-link
