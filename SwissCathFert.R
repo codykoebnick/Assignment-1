ec5875470056d7fab8309d6cdf7f87669cd34389
rafpalomino <raf.palomino@gmail.com>
2015-10-02 15:22:18 +0200

    More changes

--- SwissCathFert.R
+++ SwissCathFert.R
@@ -16,13 +16,14 @@ hist(swiss$Catholic, main = 'Catholicism in Swiss provinces (1888)', xlab = '% c
 ##This creates a histogram of the information.
 ##The functions 'main' and 'xlab' add labels to the histogram created.
 ##By using the component selector "$", the function applies to only a part of the object.
-plot(swiss$Catholic, swiss$Fertility, xlab="percentage catholic", 
-     ylab="Fertility", main="Rate of catholicism compared with fertility")
 
-## too many outliers to determine relationship
-plot(swiss$Catholic, swiss$Education)
+plot(swiss$Catholic, swiss$Fertility, xlab='Percentage catholic', ylab='Fertility', main='Rate of catholicism compared with fertility')
 
-var(swiss)
+##The distribution seems to be bimodal and does not tell us much about the relationship Catholicism and fertility.
+##We assume that provinces are either highly Catholic or highly Protestant, with no clear effect on fertility.
+
+var(swiss) ##Computes the variance of swiss
 
 ### Add dynamic link to another file 
 ### example - source(ChickWeight.r)
+```
